c061ddf5eabfc9c8d20261cc1d9ac17a20515501
Leo Gaube <gaubeleo@gmail.com>
2022-10-21 17:14:26 +0200

    display grid environment - improved representation

--- environment.py
+++ environment.py
@@ -1,8 +1,13 @@
 from shapes import *
+from buildings import *
 
 from dataclasses import dataclass
 import json
 import os
+
+# create a nice output when displaying the entire grid
+np.set_printoptions(edgeitems=30, linewidth=100000, 
+    formatter=dict(float=lambda x: "%.3g" % x))
 
 
 class Environment:
@@ -13,35 +18,60 @@ class Environment:
         self.products = products
 
         self.buildings = []
+        self.grid = np.full((height, width), ' ')
 
     def add_building(self, building: Building):
         if self.is_legal_position(building):
             self.buildings.append(building)
+            
+            # calculate building position relative to the center of the shape
+            shape_x = building.position[0] - building.shape.center[0]
+            shape_y = building.position[1] - building.shape.center[1]
+            
+            height, width = building.shape.elements.shape
+            for y_offset in range(height):
+                for x_offset in range(width):
+                    # free space can be ignored
+                    if building.shape.elements[y_offset, x_offset] == ' ':
+                        continue
+                    
+                    x = shape_x + x_offset
+                    y = shape_y + y_offset
+                    self.grid[y, x] = building.shape.elements[y_offset, x_offset]
+                    
             return True
         return False
 
-    def is_out_of_bounds(self, building: Building):
-        # calculate building position relative to the entire shape
+    def is_legal_position(self, building: Building):
+        # calculate building position relative to the center of the shape
         shape_x = building.position[0] - building.shape.center[0]
         shape_y = building.position[1] - building.shape.center[1]
         
         height, width = building.shape.elements.shape
         for y_offset in range(height):
             for x_offset in range(width):
-                if building.shape.elements[y_offset, x_offset] == 0:  # free space
+                # free space can be ignored
+                if building.shape.elements[y_offset, x_offset] == ' ':
                     continue
-                y = shape_y + y_offset
+                
                 x = shape_x + x_offset
-                if y < 0 or y >= self.height or x < 0 or x >= self.width:
-                    return True
-        return False
-
-    def is_legal_position(self, building: Building):
-        if self.is_out_of_bounds(building):
+                y = shape_y + y_offset
+                
+                # check for empty positions of every individual element
+                if not self.is_tile_empty(x, y):
+                    return False
+                
+                # ToDo: check_constraints (mines cannot be placed next to other mines)
+        return True
+    
+    def is_tile_empty(self, x, y):
+        # check whether tile is out of grid bounds
+        if y < 0 or y >= self.height or x < 0 or x >= self.width:
             return False
-        # ToDo:
-        # check_building_collisions
-        # check_constraints (mines cannot be placed next to other mines)
+        
+        # check for other buildings
+        if self.grid[y, x] != ' ':
+            return False
         return True
 
     @staticmethod
@@ -55,19 +85,23 @@ class Environment:
         for obj in task["objects"]:
             position = (obj["x"], obj["y"])
             if obj["type"] == "obstacle":
-                building = Obstacle(position)  # ToDo: width, height
+                building = Obstacle(position, obj["width"], obj["height"])
             elif obj["type"] == "deposit":
-                building = Deposit(position, obj["subtype"])
+                building = Deposit(position, obj["width"], obj["height"], obj["subtype"])
             elif obj["type"] == "factory":
                 building = Factory(position, obj["subtype"])
             elif obj["type"] == "conveyor":
                 building = Conveyor(position, obj["subtype"])
             else:
-                print(f"UNKNOWN BUILDING: {obj['type']}\n")
+                print(f"UNKNOWN BUILDING TYPE: {obj['type']}\n")
+                continue
 
-            env.add_building(building)
-            print(building)
-            print()
+            if env.is_legal_position(building):
+                env.add_building(building)
+                print(building)
+                print()
+            else:
+                print(f"UNABLE TO PLACE {obj['type']} at position {position}\n")
         return env
 
     @staticmethod
@@ -75,60 +109,13 @@ class Environment:
         pass
 
     def __repr__(self):
-        pass
-        # ToDo: nice presentation of the entire grid
+        return f"\nCOMPLETE ENVIRONMENT:\n\n{self.grid}\n".replace("'", "")
 
-
-class Obstacle(Building):
-    def __init__(self, position):
-        super().__init__(position, OBSTACLE_SHAPE)
-
-
-class Deposit(Building):
-    def __init__(self, position, subtype):
-        super().__init__(position, DEPOSIT_SHAPE)
-
-        # width/height should be adjustable in real game
-        self.width = 3
-        self.height = 3
-
-        self.subtype = subtype
-        self.resources[subtype] = self.width * self.height * 5
-
-
-class Factory(Building):
-    def __init__(self, position, subtype):
-        super().__init__(position, FACTORY_SHAPE)
-
-        self.subtype = subtype
-
-        # product_recipe, points --> should be managed by environment or factory class?!
-
-
-class Conveyor(Building):
-    def __init__(self, position, subtype):
-        self.subtype = subtype
-        if subtype == 0:
-            shape = CONVEYOR_SHAPE_0
-        elif subtype == 1:
-            shape = CONVEYOR_SHAPE_1
-        elif subtype == 2:
-            shape = CONVEYOR_SHAPE_2
-        elif subtype == 3:
-            shape = CONVEYOR_SHAPE_3
-        elif subtype == 4:
-            shape = CONVEYOR_SHAPE_4
-        elif subtype == 5:
-            shape = CONVEYOR_SHAPE_5
-        elif subtype == 6:
-            shape = CONVEYOR_SHAPE_6
-        elif subtype == 7:
-            shape = CONVEYOR_SHAPE_7
-
-        super().__init__(position, shape)
 
 
 if __name__ == "__main__":
     # filename = os.path.join(".", "tasks", "001.task.json")
     filename = os.path.join(".", "tasks", "manual solutions", "task_1.json")
     env = Environment.from_json(filename)
+    
+    print(env)
--- shapes.py
+++ shapes.py
@@ -8,49 +8,45 @@ class Shape:
     elements: np.ndarray
     
     def __repr__(self):
-        a = np.where(self.elements == 0, " ", self.elements)
-        a = np.where(self.elements == 1, "0", a)
-        a = np.where(self.elements == 2, "+", a)
-        a = np.where(self.elements == 3, "-", a)
-        a[self.center[1], self.center[0]] = "*"
+        # mark center of the shape with *
+        a = np.copy(self.elements)
+        a[self.center[1], self.center[0]] = '*'
         return f"\n{a}"
 
-@dataclass(frozen=True, order=True)   
-class Building:
-    position: tuple
-    shape: Shape
-    resources = [0]*8
 
-# 0 = free space
-# 1 = neural element
-# 2 = input element
-# 3 = output element
+# ' ' = free space
+# 'd', 'f', 'x', '<', '>', '^', 'v' = neural element
+# '+' = input element
+# '-' = output element
 
 FACTORY_SHAPE = Shape(
     (0, 0),
     np.array(
         [
-            [3, 3, 3, 3, 3],
-            [3, 1, 1, 1, 3],
-            [3, 1, 1, 1, 3],
-            [3, 1, 1, 1, 3],
-            [3, 1, 1, 1, 3],
-            [3, 3, 3, 3, 3],
+            ['+', '+', '+', '+', '+'],
+            ['+', 'f', 'f', 'f', '+'],
+            ['+', 'f', 'f', 'f', '+'],
+            ['+', 'f', 'f', 'f', '+'],
+            ['+', '+', '+', '+', '+'],
         ]
     ),
 )
 
-CONVEYOR_SHAPE_0 = Shape((1, 0), np.array([[3, 1, 2]]))
-CONVEYOR_SHAPE_1 = Shape((0, 1), np.array([[3], [1], [2]]))
-CONVEYOR_SHAPE_2 = Shape((1, 0), np.array([[2, 1, 3]]))
-CONVEYOR_SHAPE_3 = Shape((0, 1), np.array([[2], [1], [3]]))
-CONVEYOR_SHAPE_4 = Shape((1, 0), np.array([[3, 1, 1, 2]]))
-CONVEYOR_SHAPE_5 = Shape((0, 1), np.array([[3], [1], [1], [2]]))
-CONVEYOR_SHAPE_6 = Shape((1, 0), np.array([[2, 1, 1, 3]]))
-CONVEYOR_SHAPE_7 = Shape((0, 1), np.array([[2], [1], [1], [3]]))
+CONVEYOR_SHAPE_0 = Shape((1, 0), np.array([['+', '>', '-']]))
+CONVEYOR_SHAPE_1 = Shape((0, 1), np.array([['+'], ['v'], ['-']]))
+CONVEYOR_SHAPE_2 = Shape((1, 0), np.array([['-', '<', '+']]))
+CONVEYOR_SHAPE_3 = Shape((0, 1), np.array([['-'], ['^'], ['+']]))
+CONVEYOR_SHAPE_4 = Shape((1, 0), np.array([['+', '>', '>', '-']]))
+CONVEYOR_SHAPE_5 = Shape((0, 1), np.array([['+'], ['v'], ['v'], ['-']]))
+CONVEYOR_SHAPE_6 = Shape((1, 0), np.array([['-', '<', '<', '+']]))
+CONVEYOR_SHAPE_7 = Shape((0, 1), np.array([['-'], ['^'], ['^'], ['+']]))
 
-# ToDo: shape should be adjustable for obstacle/deposit
-# Placeholder:
-OBSTACLE_SHAPE = Shape((0, 0), np.array([[1]]))
-DEPOSIT_SHAPE = Shape((0, 0), np.array([[2, 2, 2], [2, 1, 2], [2, 2, 2]]))
 
+def obstacle_shape(width, height):
+    array = np.full((height, width), 'x')
+    return Shape((0, 0), array)
+
+def deposit_shape(width, height):
+    array = np.full((height, width), '-')
+    array[1:(height-1), 1:(width-1)] = 'd'
+    return Shape((0, 0), array)
