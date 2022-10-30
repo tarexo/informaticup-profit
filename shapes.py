c2df00170dc7773a0929f8b4872623f7c22e34cd
tarexo <koschinski.y@gmail.com>
2022-10-30 15:33:41 +0100

    Remove conveyor, miner and combiner shapes from shapes.py

--- shapes.py
+++ shapes.py
@@ -32,26 +32,6 @@ FACTORY_SHAPE = Shape(
     ),
 )
 
-CONVEYOR_SHAPE_0 = Shape((1, 0), np.array([['+', '>', '-']]))
-CONVEYOR_SHAPE_1 = Shape((0, 1), np.array([['+'], ['v'], ['-']]))
-CONVEYOR_SHAPE_2 = Shape((1, 0), np.array([['-', '<', '+']]))
-CONVEYOR_SHAPE_3 = Shape((0, 1), np.array([['-'], ['^'], ['+']]))
-CONVEYOR_SHAPE_4 = Shape((1, 0), np.array([['+', '>', '>', '-']]))
-CONVEYOR_SHAPE_5 = Shape((0, 1), np.array([['+'], ['v'], ['v'], ['-']]))
-CONVEYOR_SHAPE_6 = Shape((1, 0), np.array([['-', '<', '<', '+']]))
-CONVEYOR_SHAPE_7 = Shape((0, 1), np.array([['-'], ['^'], ['^'], ['+']]))
-
-
-MINE_SHAPE_0 = Shape((1, 0), np.array([[' ', 'm', 'm', ' '], ['+', 'm', 'm', '-']]))
-MINE_SHAPE_1 = Shape((0, 1), np.array([['+', ' '], ['m', 'm'], ['m', 'm'], ['-', ' ']]))
-MINE_SHAPE_2 = Shape((1, 0), np.array([['-', 'm', 'm', '+'], [' ', 'm', 'm', ' ']]))
-MINE_SHAPE_3 = Shape((0, 1), np.array([[' ', '-'], ['m', 'm'], ['m', 'm'], [' ', '+']]))
-
-COMBINER_SHAPE_0 = Shape((1, 1), np.array([['+', 'c', ' '], ['+', 'c', '-'], ['+', 'c', ' ']]))
-COMBINER_SHAPE_1 = Shape((1, 1), np.array([['+', '+', '+'], ['c', 'c', 'c'], [' ', '-', ' ']]))
-COMBINER_SHAPE_2 = Shape((1, 1), np.array([[' ', 'c', '+'], ['-', 'c', '+'], [' ', 'c', '+']]))
-COMBINER_SHAPE_3 = Shape((1, 1), np.array([[' ', '-', ' '], ['c', 'c', 'c'], ['+', '+', '+']]))
-
 def obstacle_shape(width, height):
     array = np.full((height, width), 'x')
     return Shape((0, 0), array)
