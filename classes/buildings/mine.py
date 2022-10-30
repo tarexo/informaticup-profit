69fb5a4cb35e6d72cdea9b5061a73ceeba36a991
tarexo <koschinski.y@gmail.com>
2022-10-30 18:30:43 +0100

    Add docstring for mine.py

--- classes/buildings/mine.py
+++ classes/buildings/mine.py
@@ -2,6 +2,21 @@ from .building import Building
 from shapes import *
 
 class Mine(Building):
+    """A mine is the only way extracting resources from a deposit.
+
+    Inherits from class Building.
+
+    Attributes
+    ----------
+    position : tuple
+        The position of the building in (x,y)
+    shape : Shape
+        The shape of the building
+    resources : list
+        The resources currently held by the building
+    subtype : int
+        The subtype of the mine, determining its rotation (0-3)
+    """
     def __init__(self, position, subtype):
         self.subtype = subtype
 
