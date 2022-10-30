2890662d625cbed3232a9ed43e317d963700fbe9
tarexo <koschinski.y@gmail.com>
2022-10-30 18:29:43 +0100

    Add docstring for factory.py

--- classes/buildings/factory.py
+++ classes/buildings/factory.py
@@ -2,6 +2,21 @@ from .building import Building
 from shapes import *
 
 class Factory(Building):
+    """A factory produces products using the resources it receives.
+
+    Inherits from class Building.
+
+    Attributes
+    ----------
+    position : tuple
+        The position of the building in (x,y)
+    shape : Shape
+        The shape of the building
+    resources : list
+        The resources currently held by the building
+    subtype : int
+        The subtype of the factory, determining the product (0-7)
+    """
     def __init__(self, position, subtype):
         super().__init__(position, FACTORY_SHAPE)
 
