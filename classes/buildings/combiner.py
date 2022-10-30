061e5a7974ca0cd7fabef8a90cbbabb7b21fda93
tarexo <koschinski.y@gmail.com>
2022-10-30 18:21:47 +0100

    Add docstring for combiner.py

--- classes/buildings/combiner.py
+++ classes/buildings/combiner.py
@@ -2,6 +2,21 @@ from .building import Building
 from shapes import *
 
 class Combiner(Building):
+    """The combiner has multiple inputs and combines them in one output
+
+    Inhertis from class Building.
+
+    Attributes
+    ----------
+    position : tuple
+        The position of the building in (x,y)
+    shape : Shape
+        The shape of the building
+    resources : list
+        The resources currently held by the building
+    subtype : int
+        The subtype of the combiner, determining its rotation (0-3)
+    """
     def __init__(self, position, subtype):
         self.subtype = subtype
 
