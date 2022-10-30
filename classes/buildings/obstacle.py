562f9a5e71567673cfad772b204eac314afbc9ac
tarexo <koschinski.y@gmail.com>
2022-10-30 18:32:41 +0100

    Add docstring for obstacle.py

--- classes/buildings/obstacle.py
+++ classes/buildings/obstacle.py
@@ -2,5 +2,16 @@ from shapes import *
 from .building import Building
 
 class Obstacle(Building):
+    """An obstacle is an inanimate object on the grid placed by the game, not by the player. Other buildings cannot be placed on it.
+
+    Inherits from class Building.
+
+    Attributes
+    ----------
+    position : tuple
+        The position of the building in (x,y)
+    shape : Shape
+        The shape of the building
+    """
     def __init__(self, position, width, height):
         super().__init__(position, obstacle_shape(width, height))
