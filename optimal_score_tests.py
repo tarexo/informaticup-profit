bcd2718c369e702d180951e460a5597d315bb418
debinkli <lisabinkert73@gmail.com>
2022-11-01 14:46:31 +0100

    add tests and correct bug

---
+++ optimal_score_tests.py
@@ -0,0 +1,29 @@
+import unittest
+import os
+from environment import Environment
+import optimal_score as opt
+
+class Optimal_Score_Tests(unittest.TestCase):
+
+    def test_scores_of_environments(self):
+        filename = os.path.join(".", "tasks", "001.task.json")
+        env = Environment.from_json(filename)
+        self.assertEqual(opt.optimal_score(env),410)
+
+        filename = os.path.join(".", "tasks", "002.task.json")
+        env = Environment.from_json(filename)
+        self.assertEqual(opt.optimal_score(env),120)
+        
+        filename = os.path.join(".", "tasks", "003.task.json")
+        env = Environment.from_json(filename)
+        self.assertEqual(opt.optimal_score(env),60)
+        
+        filename = os.path.join(".", "tasks", "004.task.json")
+        env = Environment.from_json(filename)
+        self.assertEqual(opt.optimal_score(env),720)
+        
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
