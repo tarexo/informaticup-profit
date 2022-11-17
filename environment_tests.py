915e7d74aa7d7542905bce7474699724ea16120b
debinkli <lisabinkert73@gmail.com>
2022-11-17 08:05:00 +0100

    fixed environment unittests
    all pass

--- environment_tests.py
+++ environment_tests.py
@@ -3,12 +3,13 @@ import os
 from environment import Environment
 import classes.buildings as buildings
 import numpy as np
+import helper.functions.file_handler as fh
 
 
 class Environment_Tests(unittest.TestCase):
     def test_initialize_environment(self):
         filename = os.path.join(".", "tasks", "002.task.json")
-        env = Environment.from_json(filename)
+        env = fh.environment_from_json(filename)
         with open("env_test_task002.npy", "rb") as f:
             expected = np.load(f)
         np.testing.assert_array_equal(env.grid, expected)
@@ -18,7 +19,7 @@ class Environment_Tests(unittest.TestCase):
         self.assertEqual(env.grid.shape, expected.shape)
 
         filename = os.path.join(".", "tasks", "003.task.json")
-        env = Environment.from_json(filename)
+        env = fh.environment_from_json(filename)
 
         with open("env_test_task003.npy", "rb") as f:
             expected = np.load(f)
@@ -26,91 +27,119 @@ class Environment_Tests(unittest.TestCase):
         self.assertEqual(env.grid.shape, expected.shape)
 
     def test_placing_buildings(self):
+        # place building at empty field
         filename = os.path.join(".", "tasks", "002.task.json")
-        env2 = Environment.from_json(filename)
-        filename = os.path.join(".", "tasks", "003.task.json")
-        env3 = Environment.from_json(filename)
+        env2 = fh.environment_from_json(filename)
         factory = buildings.Factory((0, 0), 1)
-        self.assertFalse(env2.add_building(factory))
+        self.assertEqual(
+            env2.add_building(factory),
+            None,
+            "A factory should not be allowed at the same position as a deposit",
+        )
 
-        factory = buildings.Factory((10, 10), 1)
-        self.assertTrue(env3.add_building(factory))
-        self.assertFalse(env3.add_building(factory))
+        # place twice a factory
+        filename = os.path.join(".", "tasks", "003.task.json")
+        env3 = fh.environment_from_json(filename)
+        factory1 = buildings.Factory((10, 10), 1)
+        self.assertEqual(
+            env3.add_building(factory1),
+            factory1,
+            "A factory sould be allowd to be placed on an empty field",
+        )
+        factory2 = buildings.Factory((10, 10), 1)
+        self.assertEqual(
+            env3.add_building(factory2),
+            None,
+            "A factory can not be placed twice at the same position",
+        )
 
         # mine subtye
-        env3 = Environment.from_json(filename)
+        env3 = fh.environment_from_json(filename)
         mine = buildings.Mine((8, 2), 0)
-        self.assertFalse(
-            env3.add_building(mine), "mines are not allowed to overlap with deposits"
-        )
-        env3 = Environment.from_json(filename)
-        mine = buildings.Mine((8, 2), 1)
-        self.assertTrue(
+        self.assertEqual(
             env3.add_building(mine),
+            None,
+            "mines are not allowed to overlap with deposits",
+        )
+        env3 = fh.environment_from_json(filename)
+        mine = buildings.Mine((8, 2), 1)
+        self.assertEqual(
+            env3.add_building(mine),
+            mine,
             "mine should be allowed to be build next to deposits",
         )
 
     def test_building_constrains(self):
         filename = os.path.join(".", "tasks", "003.task.json")
         # mine next to combiner
-        env3 = Environment.from_json(filename)
+        env3 = fh.environment_from_json(filename)
         mine = buildings.Mine((9, 4), 0)
-        self.assertTrue(env3.add_building(mine))
+        self.assertEqual(env3.add_building(mine), mine)
         combiner = buildings.Combiner((9, 7), 0)
-        self.assertFalse(env3.add_building(combiner))
+        self.assertEqual(env3.add_building(combiner), None)
 
         # mine next to mine
-        env3 = Environment.from_json(filename)
-        self.assertTrue(env3.add_building(mine))
+        env3 = fh.environment_from_json(filename)
+        self.assertEqual(env3.add_building(mine), mine)
         mine2 = buildings.Mine((13, 4), 0)
-        self.assertFalse(env3.add_building(mine2))
+        self.assertEqual(env3.add_building(mine2), None)
 
         # combiner to deposit
-        env3 = Environment.from_json(filename)
+        env3 = fh.environment_from_json(filename)
         combiner = buildings.Combiner((9, 5), 3)
-        self.assertFalse(
-            env3.add_building(combiner), "combiners are not allowed next to deposits"
+        self.assertEqual(
+            env3.add_building(combiner),
+            None,
+            "combiners are not allowed next to deposits",
         )
 
         # conveyor to deposit
-        env3 = Environment.from_json(filename)
+        env3 = fh.environment_from_json(filename)
         conveyor = buildings.Combiner((9, 1), 0)
-        self.assertFalse(
-            env3.add_building(conveyor), "conveyors are not allowed next to deposits"
+        self.assertEqual(
+            env3.add_building(conveyor),
+            None,
+            "conveyors are not allowed next to deposits",
         )
 
         # factory to deposit
-        env3 = Environment.from_json(filename)
+        env3 = fh.environment_from_json(filename)
         factory = buildings.Combiner((8, 2), 0)
-        self.assertFalse(
-            env3.add_building(factory), "factories are not allowed next to deposits"
+        self.assertEqual(
+            env3.add_building(factory),
+            None,
+            "factories are not allowed next to deposits",
         )
 
         # factory to factory
-        env3 = Environment.from_json(filename)
+        env3 = fh.environment_from_json(filename)
         factory1 = buildings.Combiner((12, 1), 0)
-        self.assertTrue(env3.add_building(factory1))
+        self.assertEqual(env3.add_building(factory1), factory1)
         factory2 = buildings.Combiner((17, 1), 0)
-        self.assertTrue(
+        self.assertEqual(
             env3.add_building(factory2),
+            factory2,
             "factories should be allowed to be build next to a facotry",
         )
 
         # deposit->mine->conveyor->conveyor
-        env3 = Environment.from_json(filename)
-        mine = buildings.Mine((8, 5), 3)
-        self.assertTrue(
+        env3 = fh.environment_from_json(filename)
+        mine = buildings.Mine((6, 9), 1)
+        self.assertEqual(
             env3.add_building(mine),
+            mine,
             "mines should be allowed to be build next to a deposits",
         )
-        conv1 = buildings.Conveyor((10, 7), 0)
-        self.assertTrue(
+        conv1 = buildings.Conveyor((8, 11), 0)
+        self.assertEqual(
             env3.add_building(conv1),
+            conv1,
             "conveyors should be allowed to be build next to a mines",
         )
-        conv2 = buildings.Conveyor((13, 7), 0)
-        self.assertTrue(
+        conv2 = buildings.Conveyor((11, 11), 4)
+        self.assertEqual(
             env3.add_building(conv2),
+            conv2,
             "conveyors should be allowed to be build next to a conveyors",
         )
 
