88a4f0968b76e99644bd3b308cb1247c73a3bebd
tarexo <koschinski.y@gmail.com>
2022-11-18 19:49:13 +0100

    Add start and end round action to deposit

--- classes/buildings/deposit.py
+++ classes/buildings/deposit.py
@@ -1,4 +1,5 @@
 from .unplacable_building import UnplacableBuilding
+import numpy as np
 
 
 class Deposit(UnplacableBuilding):
@@ -38,3 +39,26 @@ class Deposit(UnplacableBuilding):
             "height": self.height,
         }
         return building_dict
+
+    def start_of_round_action(self, round):
+        return
+
+    def end_of_round_action(self, round):
+        if np.max(self.resources) == 0 or len(self.connections) == 0:
+            return
+
+        indices = np.array([i for i in range(len(self.connections))])
+        for i in indices:
+            self.connections[i].resource_cache[self.subtype] += (
+                3 if self.resources[self.subtype] >= 3 else self.resources[self.subtype]
+            )
+            took = (
+                3 if self.resources[self.subtype] >= 3 else self.resources[self.subtype]
+            )
+            print(
+                f"{round} (end): ({self.x},{self.y}) takes {took}x{i}, {self.resources[self.subtype] - took}x{i} available"
+            )
+            self.resources[self.subtype] -= 3
+            if self.resources[self.subtype] <= 0:
+                break
+        return
