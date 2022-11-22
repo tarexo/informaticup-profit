5b7bba9c0450b4aaa0c1b263391d78a4252de456
tarexo <koschinski.y@gmail.com>
2022-11-22 11:24:59 +0100

    Add docstring to simulation logs

--- helper/functions/simulation_logs.py
+++ helper/functions/simulation_logs.py
@@ -2,6 +2,14 @@ from helper.constants.settings import PRINT_ROUND_LOG
 
 
 def log_start_round(building, round, store_indices, cache_indices):
+    """Generates and prints the start of round log for a given building if PRINT_ROUND_LOG is set.
+
+    Args:
+        building (Building): The building which requests a log print.
+        round (int): Current round.
+        store_indices (np.array): Array with indices where building.resources is non-zero.
+        cache_indices (np.array): Array with indices where building.resource_cache is non-zero.
+    """
     if not PRINT_ROUND_LOG:
         return
     str = f"{round} (start): ({building.x},{building.y}) accepts ["
@@ -17,6 +25,13 @@ def log_start_round(building, round, store_indices, cache_indices):
 
 
 def log_factory_end_round(building, round, points):
+    """Generates and prints the factory end of round log if PRINT_ROUND_LOG is set.
+
+    Args:
+        building (Building): The factory instance which requests a log print.
+        round (int): Current round.
+        points (int): Points of the produced product.
+    """
     if not PRINT_ROUND_LOG:
         return
     str = f"{round} (end): ({building.x},{building.y}) produces {building.subtype} ({points} points)"
@@ -24,6 +39,13 @@ def log_factory_end_round(building, round, points):
 
 
 def log_deposit_end_round(building, round, takes_out):
+    """Generates and prints the deposit end of round log if PRINT_ROUND_LOG is set.
+
+    Args:
+        building (Building): The deposit instance which requests a log print.
+        round (int): Current round.
+        takes_out (int): Number of items taken out of the deposit.resources array.
+    """
     if not PRINT_ROUND_LOG:
         return
     str = f"{round} (end): ({building.x},{building.y}) takes [{takes_out}x{building.subtype}], [{building.resources[building.subtype] - takes_out}x{building.subtype}] available"
