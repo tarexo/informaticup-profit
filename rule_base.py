adb7db1f09e00ff84ebe9555fe3083f68def9026
debinkli <lisabinkert73@gmail.com>
2022-12-03 11:55:34 +0100

    add factory

--- rule_base.py
+++ rule_base.py
@@ -7,21 +7,31 @@ import helper.functions.file_handler as fh
 
 def generate_solution(filename):
     env = fh.environment_from_json(filename)
-    print(env.grid)
 
     add_factory(env)
-
-    add_mine(env)
-
-
-def add_factory(env):
     print(env.grid)
+    add_mine(env)
+    print(env.grid)
+
+
+def add_factory(env: Environment):
+    for i in range(len(env.grid) - 1):
+        for j in range(len(env.grid[i]) - 1):
+            factory = buildings.Factory((j, i), 0)
+            success = env.add_building(factory)
+            if success == factory:
+                return
 
 
 def add_mine(env):
-    print(env.grid)
+    deposit = None
+    for building in env.buildings:
+        if type(building) == buildings.Deposit:
+            deposit = building
+    if deposit == None:
+        return
 
 
 if __name__ == "__main__":
     filename = os.path.join(".", "tasks", "002.task.json")
-    generate_solution()
+    generate_solution(filename)
