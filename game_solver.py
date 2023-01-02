2b60a4ef8fdf07c3672e92ba10e60998f2f0ee4a
debinkli <lisabinkert73@gmail.com>
2023-01-02 14:35:47 +0100

    bugfixes

--- game_solver.py
+++ game_solver.py
@@ -13,39 +13,40 @@ class GameSolver:
         self.env = env
         #self.orderd_products = self.get_product_order()
         self.score_list= optimal_score(env)
-        print(self.optimal_score)
-        self.solve()
+        self.env_resources = get_env_resources(env)
+        self.all_deposits= get_deposits(env)
+    
 
         
     def solve(self):
         print('solve Game')
         #build pairs
-        for product_list in self.score_list:
-            products_scores = []
-            for p in product_list[1]:
-                score = calc_best_score_of_single_product(env, p, self.env.turns -2, self.env.resources)
-                products_scores.append([score,p])
-            products_scores = self.sort_product_list(products_scores)
+        product_list = self.score_list[0]
+        products_scores = []
+        for p in product_list[1]:
+            score = calc_best_score_of_single_product(env, p, self.env.turns -2, self.env_resources)
+            products_scores.append([score,p])
+        products_scores = self.sort_product_list(products_scores)
 
-            for p in products_scores:
-                deposits = []
-                all_deposits= get_deposits(env)
-                index = np.argwhere(p.resouces>0)
-                for d in all_deposits:
-                    if d.subtype in index:
-                        deposits.append(d)
-                connected = self.solve_product(p,deposits)
-                if not connected : break 
+        for _,p in products_scores:
+            deposits = []
+            
+            index = np.argwhere(p.resources>0)
+            for d in self.all_deposits:
+                if d.subtype in index:
+                    deposits.append(d)
+            connected = self.solve_product(p,deposits)
+            if not connected : break 
             
     
     def solve_product(self, product:Product, deposits):
         min_connections = np.count_nonzero(product.resources)
         #build factory
-        factory_positions = get_all_factory_positions(self.env)
+        factory_positions = get_all_factory_positions(self.env,self.all_deposits)
         #build mine
         connected = False
         for deposit in deposits:
-            for factory_pos in factory_positions():
+            for factory_pos in factory_positions:
                 factory = buildings.Mine((factory_pos[0], factory_pos[1]), product.subtype)
                 if factory == None: continue
                 connected = self.make_connection(deposit, factory)
@@ -68,7 +69,13 @@ class GameSolver:
         return False
 
     def update(self, product):
-        print('to be done')
+        new_score_list = []
+        for products in self.score_list:
+            if product not in products:
+                new_score_list.append(products)
+        self.score_list = new_score_list
+        self.solves
+
         #update product order if one or more pairs don't work
 
 
@@ -77,7 +84,7 @@ class GameSolver:
         sorted = order.copy()
         nums = np.zeros(len(order))
         for i in range(len(order)):
-            nums[i] = order[i][1]
+            nums[i] = order[i][0]
             
         args = np.argsort(nums)
         for i in range(len(args)):
@@ -93,11 +100,14 @@ class GameSolver:
 if __name__ == "__main__":
     filename = os.path.join(".", "tasks","003.task.json")#tasks\hard\profit.task.1671032210813.json
     env = fh.environment_from_json(filename)
-    deposits = get_deposits(env)
+    solver = GameSolver(env)
+    solver.solve()
+
+    '''deposits = get_deposits(env)
     for deposit in deposits:
         mine_positions = get_all_mines_positions(env, deposit)
         for x,y,sub in mine_positions:
-            print(str(x)+', '+str(y)+':  '+str(sub))
+            print(str(x)+', '+str(y)+':  '+str(sub))'''
     #factory_positions = get_all_factory_positions(env,deposits)
     #for a in factory_positions:
         #print(a)
